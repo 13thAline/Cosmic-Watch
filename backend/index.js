8033345c3eebb02ef7d5204e8cc340b0a7081985
BibhuDev <bibhuduttapanda7@gmail.com>
2026-02-07 14:05:25 +0530

    database connection successful

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const dotenv= require('dotenv');
 dotenv.config();
+const connectToDB = require('./database/db');
 
 const app = express();
+
+connectToDB();
 
 const PORT= process.env.PORT || 5000;
 app.listen(PORT, ()=>{
